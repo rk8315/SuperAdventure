7d51696802519c6ca83590512cad6d1ff61255e8
Bob K <kearnsr135@gmail.com>
2021-10-04 12:41:26 -0400

    TestCommit1

--- SuperAdventure/Program.cs
+++ SuperAdventure/Program.cs
@@ -14,6 +14,7 @@ namespace SuperAdventure
         [STAThread]
         static void Main()
         {
+            // test
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new SuperAdventure());
